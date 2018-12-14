e9ba6368e215a712364258ba73d3a05c0723ac96
Chris Harding <chrish@cantab.net>
2018-12-14 12:33:53 +0000

    Wiped over with my (cleaner) solution. Setting epsilon to 0 gives better results, suprisingly. Baseline score after 20k episodes is around 9.3 - 9.5, which I think is the best the current submission can actually do anyway.

--- agent.py
+++ agent.py
@@ -1,33 +1,58 @@
 import numpy as np
-from collections import deque, defaultdict
-import random, sys, math, gym
+from collections import defaultdict
+import random
+import sys
+import math
+import gym
+
 
 class Agent:
 
     def __init__(self, nA=6):
+
         self.nA = nA
-        self.Q = defaultdict(lambda: np.zeros(self.nA))
-        self.epsilon = 0.1
-        self.alpha = 0.1
-        self.gamma = 0.8
-        self.episode = 1
+        self.Q = defaultdict(lambda: np.ones(self.nA) * 0.0)
 
-    def select_action(self, state, i_episode, num_episodes):
-        if i_episode != self.episode and i_episode % (num_episodes * 0.0005) == 0:
-            self.epsilon -= 50/num_episodes
+        # Learning rate / step size
+        self.alpha = 1
+        self.alpha_decay = 0.999
+        self.alpha_min = 0.01
 
-        if i_episode != self.episode and i_episode % (num_episodes * 0.005) == 0:
-            self.alpha -= 20/num_episodes
-            self.epsilon = 0.8
+        # Discount
+        self.gamma = 1
+        self.gamma_decay = 1
+        self.gamma_min = 1
 
-        if i_episode != self.episode and i_episode % (num_episodes * 0.05) == 0:
-            self.gamma -= 20/num_episodes
-            self.epsilon = 0.8
-            self.alpha = 0.8
+        # Exploration
+        self.epsilon = 0
+        self.epsilon_decay = 1.0
+        self.epsilon_min = 0
 
-        return np.argmax(self.Q[state])
+        print("alpha: {0}, alpha_decay: {1}, alpha_min: {2}".format(
+            self.alpha, self.alpha_decay, self.alpha_min))
+        print("gamma: {0}, gamma_decay: {1}, gamma_min: {2}".format(
+            self.gamma, self.gamma_decay, self.gamma_min))
+        print("epsilon: {0}, epsilon_decay: {1}, epsilon_min: {2}".format(
+            self.epsilon, self.epsilon_decay, self.epsilon_min))
+
+    def select_action(self, state):
+
+        # Epsilon-greedy action selection
+        Q = self.Q
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.nA)
+        else:
+            return np.argmax(Q[state])
 
     def step(self, state, action, reward, next_state, done):
-        next_action = np.argmax(self.Q[state])
-        self.Q[state][action] = self.Q[state][action] + (self.alpha * (reward + \
-            (self.gamma * np.max(self.Q[next_state])) - self.Q[state][action]))
+
+        # Q-learning
+        Q = self.Q  # alias
+        target = reward + self.gamma * Q[next_state][np.argmax(Q[next_state])]
+        Q[state][action] += self.alpha * (target + Q[state][action])
+
+        # Decay exploration, step size and discount over time
+        self.alpha = min(1, max(self.alpha_min, self.alpha * self.alpha_decay))
+        self.epsilon = min(
+            1, max(self.epsilon_min, self.epsilon * self.epsilon_decay))
+        self.gamma = min(1, max(self.gamma_min, self.gamma * self.gamma_decay))
